40857239088e179dfa1a1a35952cbe926d225665
Chénais Sébastien <sebastien.chenais@live.fr>
2013-03-06 08:55:14 +0100

    modificiation de plugin et plugin platform
    construction des classes

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ Space
 Plugins powered Space Invaders
 
 TODO:
+Système de gestion des évènements (listeners): ex action lorsqu'on est touché, lorsqu'on lance un projectile... (bukkit)
+dépendance inter plugin : propriétés dependance
 
 
 
--- src/gameEngine/PluginPlatform.java
+++ src/gameEngine/PluginPlatform.java
@@ -6,51 +6,80 @@ import java.util.Map;
 /**
  * PluginPlatform is the plugin's management class.
  * <p>
- * It's supposed to manage call for plugin and respect their properties
- * (default loading, lazy loading, single loading)
+ * It's supposed to manage call for plugin and respect their properties (default
+ * loading, lazy loading, single loading)
  * <p>
  * Constructor
  * <li>public PluginPlatform()
+ * 
  * @author liebekinder
  * @see Plugin
  */
 public class PluginPlatform {
 
-	//fields
-	
-	private Map<String,Plugin> pluginList_;
-	
-	//constructors
-	
-	public PluginPlatform(){
+	// fields
+
+	private Map<String, Plugin> pluginList_;
+
+	// constructors
+
+	public PluginPlatform() {
 		pluginList_ = new HashMap<String, Plugin>();
 	}
-	
-	//getters	
-	
+
+	// getters
+
 	public Map<String, Plugin> getPluginList() {
 		return pluginList_;
 	}
-	
-	//methods
-	
+
+	// methods
+
 	/**
 	 * Plugin getPlugin(String name)
 	 * <p>
-	 * This method find the plugin linked to the name. Check whether
-	 * name is valid or not.
-	 * @param name the name of the plugin you want
+	 * This method find the plugin linked to the name. Check whether name is
+	 * valid or not.
+	 * 
+	 * @param name
+	 *            the name of the plugin you want
 	 * @return the plugin associated to the name or null
 	 * @see Plugin
 	 */
-	public Plugin getPlugin(String name){
+	public Plugin getPlugin(String name) {
 		Plugin retour = null;
-		if(name != null){
+		if (name != null) {
 			retour = pluginList_.get(name);
-		}
-		else{
-			System.err.println(name+" n'a pas été trouvé dans la liste des plugins!");
+
+			if (retour == null) {
+				retour = getPluginAdvanced(name);
+			} else {
+				System.err.println(name
+						+ " wasn't found in the plugin list!");
+			}
+		} else {
+			System.err.println("plugin's name is null");
 		}
 		return retour;
+	}
+
+	/**
+	 * Plugin getPluginAdvanced(String name)
+	 * <p>
+	 * This method find the plugin linked to the name. It is not case sensitive
+	 * 
+	 * @param name
+	 *            nom du plugin
+	 * @return the associated to the name or null
+	 */
+	private Plugin getPluginAdvanced(String name) {
+		Plugin retour = null;
+		Map<String, Plugin> clonePluginList = new HashMap<String, Plugin>();
+		for (String s : pluginList_.keySet()) {
+			clonePluginList.put(s.toLowerCase(), pluginList_.get(s));
+		}
+		retour = clonePluginList.get(name.toLowerCase());
+		return retour;
+
 	}
 }
